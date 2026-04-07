d6bce52b906a75b694afc443ff1a89df95f142ee
Andy Do <doandy@sheridancollege.ca>
2026-04-07 12:42:33 -0400

    Start of news branch

--- frontend/src/pages/news.jsx
+++ frontend/src/pages/news.jsx
@@ -6,7 +6,23 @@ export default function NewsView() {
     const [data, setData] = useState("");
     const [keyword, setKeyword] = useState("");
     const [keywords, setKeywords] = useState([""]);
-    const [articles, setArticles] = useState("");
+    //const [articles, setArticles] = useState("");
+    const articles = [
+        {
+            keyword: "AMZN",
+            url: "https://www.bloomberg.com/news/articles/2026-03-12/amazon-plans-to-shift-annual-prime-day-sale-to-june-from-july",
+            headline: "Amazon Plans to Shift Annual Prime Day Sale to June From July",
+            publisher: "Bloomberg",
+            datetime: "March 12, 2026"
+        },
+        {
+            keyword: "AMZN",
+            url: "https://www.cnet.com/tech/services-and-software/amazon-to-increase-the-price-of-ad-free-prime-video-streaming",
+            headline: "Amazon to Increase the Price of Ad-Free Prime Video Streaming",
+            publisher: "CNET",
+            datetime: "March 13, 2026"
+        }
+    ];
 
     const handleRetrieveKeywords = async (event) => {
         try {
@@ -87,6 +103,55 @@ export default function NewsView() {
         );
     }
 
+    function ArticleTable() {
+        const handleArticleAnalysis = async (keywordName) => { // Placeholder, not coded nor implemented yet
+            if (keywordName) {
+                try {
+                    const response = await fetch('http://localhost:3000/api/removeKeyword', {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify({ message: keywordName }) // Your parameters
+                    });
+                    const result = await response.json();
+                    console.log("Removal of keyword was successful: ", result.successful)
+                    if (result.successful) {
+                        const updatedKeywords = keywords.filter(keyword => keyword !== keywordName);
+                        setKeywords(updatedKeywords)
+                    }
+                    setRefresh(prev => prev + 1);
+                } catch (error) {
+                    console.error("Error calling server: ", error)
+                }
+            }
+        }
+
+        return (
+            <table class="article-table">
+                <thead>
+                    <tr>
+                        <th>Keyword</th>
+                        <th>Article</th>
+                        <th>Publisher</th>
+                        <th>Publish Date</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody id="articleTableBody">
+                    {articles.map((article) => (
+                        <tr key={article.url}>
+                            <td>{article.keyword}</td>
+                            <td><a href={article.url}>{article.headline}</a></td>
+                            <td>{article.publisher}</td>
+                            <td>{article.datetime}</td>
+                            <td><button class="btn">Analysis</button></td>
+                            {/* <td><button onClick={() => handleArticleAnalysis(articleParameter)}>Analysis</button></td> */}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        );
+    }
+
     useEffect(() => {
         console.log("Page loaded!");
         handleRetrieveKeywords();
@@ -99,6 +164,7 @@ export default function NewsView() {
             <div class="header">
                 <h1>Financial News</h1>
             </div>
+            
             <div class="grid-container">
                 <div class="grid-container-item">
                     <form onSubmit={handleAddKeyword}>
@@ -121,23 +187,6 @@ export default function NewsView() {
                     <div class="table-scroller">
                         {KeywordTable()}
                     </div>
-
-                    {/* 
-                    <div class="table-scroller">
-                        <table class="keyword-table">
-                            <tr>
-                                <th></th>
-                                <th></th>
-                            </tr>
-                            <tr>
-                                <form action="/remove-news-keyword" method="POST">
-                                    <td>AMZN</td>
-                                    <td><button class="btn" type="submit">Remove</button></td>
-                                </form>
-                            </tr>
-                        </table>
-                    </div>
-                    */}
                 </div>
 
                 <div class="column-span">
@@ -146,46 +195,10 @@ export default function NewsView() {
 
                 <div class="column-span">
                     <div class="table-scroller">
-                        <table id="articleTable" class="article-table">
-                            <thead>
-                                <tr>
-                                    <th>Keyword</th>
-                                    <th>Article</th>
-                                    <th>Publisher</th>
-                                    <th>Publish Date</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody id="articleTableBody">
-
-
-
-                                {/* 
-                                <tr>
-                                    <td>AMZN</td>
-                                    <td><a
-                                            href="https://www.bloomberg.com/news/articles/2026-03-12/amazon-plans-to-shift-annual-prime-day-sale-to-june-from-july">Amazon
-                                            Plans to Shift Annual Prime Day Sale to June From July</a></td>
-                                    <td>Bloomberg</td>
-                                    <td>March 12, 2026</td>
-                                    <td><button class="btn">Analysis</button></td>
-                                </tr>
-
-                                <tr>
-                                    <td>AMZN</td>
-                                    <td><a
-                                            href="https://www.cnet.com/tech/services-and-software/amazon-to-increase-the-price-of-ad-free-prime-video-streaming/">Amazon
-                                            to Increase the Price of Ad-Free Prime Video Streaming</a></td>
-                                    <td>CNET</td>
-                                    <td>March 13, 2026</td>
-                                    <td><button class="btn">Analysis</button></td>
-                                </tr>
-                                */}
-                            </tbody>
-                        </table>
+                        {ArticleTable()}
                     </div>
-
                 </div>
+
             </div>
         </div>
     )
