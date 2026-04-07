3b7fafc22c7fa37287b3cb60c4febb16d4ff72dd
Andy Do <doandy@sheridancollege.ca>
2026-04-07 12:00:46 -0400

    Scraper concept abandoned, start of consolidation of scraper services into pre-existing news services

--- backend/services/newsService.js
+++ backend/services/newsService.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const axios = require("axios");
+const keywords = ["Amazon", "AMZN", "Google", "GOOG", "Tesla", "TSLA"] // Hardcoded
+const API_KEY = process.env.API_KEY;
 
-const API_KEY = process.env.NEWS_API_KEY;
 // 1. Get Today's date (The "To" date)
 const today = new Date();
 const toDate = today.toISOString().split('T')[0];
@@ -12,10 +13,23 @@ thirtyDaysAgo.setDate(today.getDate() - 30);
 const fromDate = thirtyDaysAgo.toISOString().split('T')[0];
 
 
-async function getNews(ticker) {
 
-    const url = `https://finnhub.io/api/v1/company-news?symbol=${ticker}&from=${fromDate}&to=${toDate}&token=${API_KEY}`
-    console.log(`fetching news from ${fromDate} to ${toDate}`)
+async function getGeneralNews() {
+    /*
+    List of attributes in response body:
+    - category
+    - datetime
+    - headline
+    - id
+    - image
+    - related
+    - source
+    - summary
+    - url
+    */
+    const category = "general"; // values: general, forex, crypto, or merger
+    const url = `https://finnhub.io/api/v1/news?category=${category}&token=${API_KEY}`;
+    console.log(`fetching news from ${fromDate} to ${toDate}`);
     const response = await axios.get(url);
 
     return response.data.map(article => ({
@@ -24,4 +38,52 @@ async function getNews(ticker) {
     }));
 }
 
-module.exports = {getNews};
+async function getCompanyNews(ticker) {
+    /*
+    List of attributes in response body:
+    - category
+    - datetime
+    - headline
+    - id
+    - image
+    - related
+    - source
+    - summary
+    - url
+    */
+    const url = `https://finnhub.io/api/v1/company-news?symbol=${ticker}&from=${fromDate}&to=${toDate}&token=${API_KEY}`;
+    console.log(`fetching news from ${fromDate} to ${toDate}`);
+    const response = await axios.get(url);
+
+    return response.data.map(article => ({
+        headline: article.headline,
+        summary: article.summary,
+    }));
+}
+
+
+
+async function addKeyword(newKeyword, keywordArray) {
+    // Assumes that newKeyword is simply a string consisting of a word.
+    const keywordIsNew = !(keywordArray.includes(newKeyword));
+    if (keywordIsNew) {
+        keywordArray.push(newKeyword)
+        return true;
+    } else {
+        return false;
+    }
+}
+
+async function removeKeyword(keywordToRemove, keywordArray) {
+    // Assumes that keywordToRemove is simply a string consisting of a word.
+    const index = keywordArray.indexOf(keywordToRemove);
+
+    if (index > -1) {
+        keywordArray.splice(index, 1);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+module.exports = { keywords, addKeyword, removeKeyword };
